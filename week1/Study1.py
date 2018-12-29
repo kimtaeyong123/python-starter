2f92983fb34b8e0caf3a463a3d20ec5da69cc31a
kimtaeyong <tystars@naver.com>
2018-12-30 00:44:54 +0900

    1주차 소스

--- week1/Study1.py
+++ week1/Study1.py
@@ -1,11 +1,10 @@
+aFruitList = ["사과", "바나나"]
 Fruitname = input("어떤 과일을 사고 싶으세요?")
 print("{0}를 설명해드릴게요" .format(Fruitname))
-aFruitList = ["사과", "바나나"]
-for Fruitname in range(len(aFruitList)):
-    if Fruitname == "사과":
-        print("사과는 3000원입니다")
-    elif Fruitname == "바나나":
-        print("바나나는 1000원입니다")
-    else:
-        print("그 과일은 안팝니다")
+if Fruitname == aFruitList[0]:
+    print("사과는 3000원입니다")
+elif Fruitname == aFruitList[1]:
+    print("바나나는 1000원입니다")
+else:
+    print("그 과일은 안팝니다")
 
